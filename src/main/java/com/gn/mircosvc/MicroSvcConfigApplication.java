d1e20f99fbc269f8b849fcea38a4d10153cdb6f5
Gaurav Nigam <gauravjnigam@gmail.com>
2021-01-23 13:07:34 +0530

    first commit for setup config svc

---
+++ src/main/java/com/gn/mircosvc/MicroSvcConfigApplication.java
@@ -0,0 +1,13 @@
+package com.gn.mircosvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSvcConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroSvcConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gn/mircosvc/MicroSvcConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gn.mircosvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSvcConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
